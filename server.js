e0210314cca79a443e786a87cab010542bce8584
KAKERU-GOTOUMA <43anillo@gmail.com>
2025-03-11 05:46:13 +0900

    a

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');//httpsサーバー
+const WebSocket = require('ws');//websocket
+
+const app = express();
+const server = http.createServer(app); // HTTPサーバー作成
+const wss = new WebSocket.Server({ server }); // WebSocketをHTTPサーバーに統合
+
+// /静的ファイルを公開 (http://localhost:3000/)
+app.use('/', express.static(path.join(__dirname, 'html')));
+
+// WebSocketの接続処理
+wss.on('connection', (ws, req) => {
+        ws.on('close', () => {
+
+            console.log('WebSocket disconnected');
+        });
+
+
+        // 30秒ごとにPingを送る
+        const pingInterval = setInterval(() => {
+            ws.ping(); // クライアントにPingを送信
+        }, 30000); // 30秒おきにPing
+
+        // クライアントからPongを受信したとき
+        ws.on('pong', () => {});
+});
+
+
+//サーバーを起動
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is started at http://localhost:${PORT}`);
+});
+
+
+//console上での終了操作
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log('終了はqと入力');
+
+rl.on('line', (input) => {
+    if (input.trim() === 'q' || input.trim() === "ｑ") {
+        console.log('終了します。');
+        rl.close();
+        process.exit(0);
+    }
+});

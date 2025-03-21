8bb336ee1e74691ab95f8b2c271cc98d6b6e2ec2
KAKERU-GOTOUMA <43anillo@gmail.com>
2025-03-21 15:41:11 +0900

    install.shのDEST_DIRを変更し、portapad.serviceファイルをダウンロードして有効化する処理を追加

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 FILE_URL="https://portapad.kotoca.net/files/service"
 
 # Destination directory
-DEST_DIR="/opt/service"
+DEST_DIR="/opt/portapad"
 
 # Create the destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
@@ -20,3 +20,26 @@ else
     exit 1
 fi
 
+# URL of the file to download
+FILE_URL="https://portapad.kotoca.net/files/portapad.service"
+
+# Destination directory
+DEST_DIR="/etc/systemd/system"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$DEST_DIR"
+
+# Download the file
+curl -o "$DEST_DIR/$(basename $FILE_URL)" "$FILE_URL"
+
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "serviceファイルのダウンロード完了"
+else
+    echo "Curl中のエラー２"
+    exit 1
+fi
+
+sudo systemctl enable portapad.service
+
+sudo systemctl start portapad.service

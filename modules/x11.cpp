de98a020867fd5fe5366cf4c39a6fca646f74c60
KAKERU-GOTOUMA <43anillo@gmail.com>
2025-03-30 09:18:00 +0900

    X11モジュールを追加し、マウス操作の関数を実装してサービスでの呼び出しを更新

--- modules/x11.cpp
+++ modules/x11.cpp
@@ -1,1 +1,19 @@
-#include <node_api.h>
+# include <node_api.h>
+# include <X11/Xlib.h>
+# include <X11/Xutil.h>
+# include <X11/extensions/XTest.h>
+
+
+napi_value ClickMouse(napi_env env, napi_value button) {
+    Display* display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        return;
+    }
+
+    size_t argc = 1;
+    napi_value args[1];
+    int button_;
+    napi_get_value_int32(env, args[0], &button_);
+    XTestFakeButtonEvent(display, button_, 1, 0)
+    return 0;
+}
--- service.js
+++ service.js
@@ -1,4 +1,4 @@
-const autoOSDeno = require("./modules/x11");
+const inputter = require("./modules/x11");
 const readline = require('readline');
 
 let righttouch = false;
@@ -11,49 +11,49 @@ function getmessage(message) {
     console.log(massages[0]);
     
     if (massages[0] == "lefclick") {
-      autoOSDeno.ClickMouse(1);
+      inputter.ClickMouse(1);
       console.log("clicked");
       righttouch = false;
     } else if (massages[0] == "rigclick") {
       if (righttouch == false) {
-        autoOSDeno.ClickMouse(3);
+        inputter.ClickMouse(3);
         console.log("Rclicked");
       }
       righttouch = true;
     } else if (massages[0] == "cursol") {
       let mousePos = [0, 0];
-        mousePos = autoOSDeno.GetMouse();
+        mousePos = inputter.GetMouse();
         console.log(mousePos);
       if (mousePos != null && massages.length >= 1) {
         const x = mousePos[0] + Number(massages[1]) * 4;
         const y = mousePos[1] + Number(massages[2]) * 4;
         console.log(x + "," + y);
-        autoOSDeno.MoveMouse(x, y);
+        inputter.MoveMouse(x, y);
       }
     } else if (massages[0] == "scroll") {
       if (scrolled >= 3) {
         if(massages[1] == "up"){
-          autoOSDeno.ClickMouse(4);
+          inputter.ClickMouse(4);
         }else{
-          autoOSDeno.ClickMouse(5);
+          inputter.ClickMouse(5);
         }
         scrolled = 0;
       }
       scrolled++;
     } else if (massages[0] == "drag") {
-      autoOSDeno.ChangeMouse(1,1);
+      inputter.ChangeMouse(1,1);
       let mousePos = [0, 0];
-      mousePos = autoOSDeno.GetMouse();
+      mousePos = inputter.GetMouse();
       console.log(mousePos);
       if (mousePos != null && massages.length >= 1) {
         const x = mousePos[0] + Number(massages[1]) * 4;
         const y = mousePos[1] + Number(massages[2]) * 4;
         console.log(x + "," + y);
-        autoOSDeno.MoveMouse(x, y);
+        inputter.MoveMouse(x, y);
       }
     } else if (massages[0] == "end") {
-      autoOSDeno.ChangeMouse(1,0);
-      autoOSDeno.ChangeMouse(3,0);
+      inputter.ChangeMouse(1,0);
+      inputter.ChangeMouse(3,0);
     }
   }catch(e){
     console.error(e);

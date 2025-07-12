e38cbd26b052687eabc80f53d127d296ea65d1a3
GOTOUMA-KAKERU <43anillo@gmail.com>
2025-07-13 07:58:06 +0900

    ページ分け

--- setting/main.rs
+++ setting/main.rs
@@ -1,21 +1,17 @@
+// すでにあるコードを活かしつつ、UIを切り替える形に構造化する
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+
 use eframe::{egui::*, NativeOptions};
 use eframe::egui;
 use std::process::Command;
-use futures_util::future::ok;
-use std::collections::BTreeMap;
-use std::thread;
 use std::fs;
-use std::sync::{Arc, Mutex};
 use once_cell::sync::Lazy;
 use std::path::PathBuf;
-use serde::Deserialize;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-//設定ファイルの形式
 #[derive(Deserialize, Serialize, Debug, Default)]
 struct Config {
     sigserver: String,
@@ -23,46 +19,28 @@ struct Config {
     keyboard: String,
 }
 
-//APPDATAフォルダーのPORTAPADフォルダーを表す変数
 pub static APPDATA: Lazy<PathBuf> = Lazy::new(|| {
-    //C:\Users\<ユーザー名>\AppData\Roaming
     let base_dir = dirs::config_dir().expect("APPDATAが取得できませんでした");
-    // Portapadフォルダ
     let app_dir = base_dir.join("Portapad");
-    //なければ作る
     fs::create_dir_all(&app_dir).expect("Portapadフォルダが作れませんでした");
-    dirs::config_dir()
-        .expect("APPDATAが取得できませんでした")
-        .join("portapad")
+    app_dir
 });
 
 fn setup_custom_fonts(ctx: &egui::Context) {
     let mut fonts = egui::FontDefinitions::default();
-
-    // フォントファイルを読み込む
     let font_data = std::fs::read("C:\\Windows\\Fonts\\meiryo.ttc")
         .expect("フォントファイルが読み込めません");
-
-    // フォントを追加（"jp" という名前をつける）
-    fonts.font_data.insert(
-        "jp".to_owned(),
-        egui::FontData::from_owned(font_data).into(),
-    );
-
-    // 日本語表示をサポートするように上書き
-    fonts
-        .families
-        .get_mut(&egui::FontFamily::Proportional)
-        .unwrap()
-        .insert(0, "jp".to_owned()); // 優先的に使うフォントにする
-
-    fonts
-        .families
-        .get_mut(&egui::FontFamily::Monospace)
-        .unwrap()
-        .insert(0, "jp".to_owned());
-
+    fonts.font_data.insert("jp".to_owned(), egui::FontData::from_owned(font_data).into());
+    fonts.families.get_mut(&egui::FontFamily::Proportional).unwrap().insert(0, "jp".to_owned());
+    fonts.families.get_mut(&egui::FontFamily::Monospace).unwrap().insert(0, "jp".to_owned());
     ctx.set_fonts(fonts);
+}
+
+#[derive(PartialEq, Clone, Default)]
+enum AppScreen {
+    #[default]
+    Main,
+    SubWindow,
 }
 
 #[derive(Default, Clone)]
@@ -71,11 +49,12 @@ pub struct MyApp {
     pub sig_url_sec: String,
     pub key_devicename: String,
     is_recording: bool,
+    current_screen: AppScreen,
 }
 
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+impl MyApp {
+    fn show_main_ui(&mut self, ctx: &Context) {
+        CentralPanel::default().show(ctx, |ui| {
             ui.heading("シグナリングサーバー");
             ui.horizontal(|ui| {
                 ui.label("シグナリングサーバーURL：");
@@ -87,97 +66,80 @@ impl eframe::App for MyApp {
             });
 
             ui.separator();
-
             ui.heading("キーボード登録");
             ui.horizontal(|ui| {
-                ui.label("ボタンを押した後、登録したいキーボードのキーを押してください：");
-                let recording_text;
-                if self.is_recording {recording_text = "記録中...";}else{recording_text = "記録"}
+                ui.label("登録したいキーボードのキーを押してください：");
+                let recording_text = if self.is_recording { "記録中..." } else { "記録" };
                 if ui.button(recording_text).clicked() {
                     self.is_recording = true;
                     ctx.request_repaint();
-
-                    let mut child = Command::new("target/debug/setting_forkey.exe") 
+                    let mut child = Command::new("target/debug/setting_forkey.exe")
                         .spawn()
                         .expect("rawinputプロセス起動失敗");
-
-                    println!("rawinputプロセス起動しました");
-                    // 子プロセスの終了を待つ
-                    let status = child.wait().expect("プロセス待機中にエラー");
-                    println!("rawinputプロセス起動おわり");
+                    child.wait().expect("プロセス待機中にエラー");
                     let path = APPDATA.join("input_key_num.txt");
-                    match fs::read_to_string(path) {
-                        Ok(contents) => {
-                            println!("コード:\n{}", contents);
-                            self.key_devicename = contents.trim().to_string();
-                        }
-                        Err(e) => {
-                            eprintln!("エラー: {}", e);
-                        }
+                    if let Ok(contents) = fs::read_to_string(path) {
+                        self.key_devicename = contents.trim().to_string();
                     }
                     self.is_recording = false;
                     ctx.request_repaint();
                 }
             });
-            let path = APPDATA.join("input_key_num.txt");
-            let mut keypath = "未記録".to_string();
-            match fs::read_to_string(path) {
-                Ok(contents) => {
-                    keypath = contents;
-                }
-                Err(e) => {
-                }
+
+            let keypath = fs::read_to_string(APPDATA.join("input_key_num.txt")).unwrap_or("未記録".to_string());
+            ui.label(RichText::new(keypath).small());
+
+            if ui.button("キーボードのマッピングをする").clicked() {
+                self.current_screen = AppScreen::SubWindow;
             }
-            ui.label(egui::RichText::new(keypath).small());
 
-            ui.separator();
-        });
-
-        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Portapad v2.1.1");
-
-                ui.with_layout(
-                    egui::Layout::right_to_left(egui::Align::Center),
-                    |ui| {
+            TopBottomPanel::bottom("bottom_panel").show_inside(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Portapad v2.1.1");
+                    ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
                         if ui.button("キャンセル").clicked() {
                             std::process::exit(0);
                         }
                         if ui.button("保存").clicked() {
-
-                            let mut config: Config = match fs::read_to_string(&APPDATA.join("config.toml")) {
-                                Ok(toml_str) => {
-                                    toml::from_str(&toml_str).unwrap_or_else(|e| {
-                                        eprintln!("TOMLの読み込みに失敗: {}", e);
-                                        Config::default()
-                                    })
-                                }
-                                Err(_) => {
-                                    eprintln!("ない。新規作成します。");
-                                    let default_config = Config::default();
-
-                                    // TOMLに変換して保存
-                                    let toml_str = toml::to_string_pretty(&default_config).unwrap();
-                                    fs::write(&APPDATA.join("config.toml"), toml_str);
-                                    default_config
-                                }
+                            let mut config: Config = match fs::read_to_string(APPDATA.join("config.toml")) {
+                                Ok(toml_str) => toml::from_str(&toml_str).unwrap_or_default(),
+                                Err(_) => Config::default(),
                             };
-                            
-                            if !self.sig_url.is_empty() { config.sigserver = self.sig_url.to_string(); };
-                            if !self.sig_url_sec.is_empty() { config.sec_sigserver = self.sig_url_sec.to_string(); };
-                            if !self.key_devicename.is_empty() { config.keyboard = self.key_devicename.to_string(); };
-
-                            let new_toml = toml::to_string_pretty(&config).unwrap();
-                            fs::write(&APPDATA.join("config.toml"), new_toml);
+                            if !self.sig_url.is_empty() {
+                                config.sigserver = self.sig_url.clone();
+                            }
+                            if !self.sig_url_sec.is_empty() {
+                                config.sec_sigserver = self.sig_url_sec.clone();
+                            }
+                            if !self.key_devicename.is_empty() {
+                                config.keyboard = self.key_devicename.clone();
+                            }
+                            fs::write(APPDATA.join("config.toml"), toml::to_string_pretty(&config).unwrap()).unwrap();
                             std::process::exit(0);
                         }
-                    },
-                );
+                    });
+                });
             });
         });
-
     }
 
+    fn show_subwindow_ui(&mut self, ctx: &Context) {
+
+        CentralPanel::default().show(ctx, |ui| {
+            if ui.button("戻る").clicked() {
+                self.current_screen = AppScreen::Main;
+            }
+        });
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        match self.current_screen {
+            AppScreen::Main => self.show_main_ui(ctx),
+            AppScreen::SubWindow => self.show_subwindow_ui(ctx),
+        }
+    }
 }
 
 fn main() {

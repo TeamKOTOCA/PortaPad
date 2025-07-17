57e3d6c99fc96c4e83bd8ea5ac694384c91386c8
GOTOUMA-KAKERU <43anillo@gmail.com>
2025-07-17 12:05:36 +0900

    疲れた

--- test/main.rs
+++ test/main.rs
@@ -1,30 +1,20 @@
 use std::ptr::null_mut;
+use egui::mutex::Mutex;
 use windows::Win32::Foundation::*;
+use windows::Win32::Foundation::HANDLE;
 use windows::Win32::UI::WindowsAndMessaging::*;
-use windows::Win32::UI::WindowsAndMessaging::CallNextHookEx;
-use windows::Win32::UI::WindowsAndMessaging::HC_ACTION;
-use windows::Win32::System::LibraryLoader::GetModuleHandleW;
 
 use std::collections::VecDeque;
-use std::fs::File;
-use std::io::Write;
 use std::mem::{size_of, zeroed};
 use std::ffi::CString;
-use windows::Win32::Foundation::*;
 use windows::Win32::System::LibraryLoader::*;
 use windows::Win32::UI::Input::*;
-use windows::Win32::UI::WindowsAndMessaging::*;
 use windows::core::PCSTR;
-use chrono::Local;
 use windows::Win32::Graphics::Gdi::UpdateWindow;
-use tokio::time::{sleep, Duration};
 
 use once_cell::sync::Lazy;
-use std::path::PathBuf;
 use std::fs;
 
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
 use std::sync::mpsc::{Sender, Receiver, channel};
 
 
@@ -33,8 +23,7 @@ static mut KEY_SENDER: Option<Sender<u32>> = None;
 // グローバル変数（スレッド間で安全に使う）
 static mut HOOK_HANDLE: HHOOK = HHOOK(null_mut());
 
-
-pub static KEYBOARD_HANDLE: Lazy<String> = Lazy::new(|| {
+pub static KEYBOARD_PATH: Lazy<String> = Lazy::new(|| {
     let mut handle = "".to_string();
     let path = dirs::config_dir()
         .expect("APPDATAが取得できませんでした")
@@ -46,32 +35,52 @@ pub static KEYBOARD_HANDLE: Lazy<String> = Lazy::new(|| {
     handle
 });
 
-// キーボードフックコールバック
-unsafe extern "system" fn keyboard_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    if code == HC_ACTION as i32 {
-        let kbd_struct = *(lparam.0 as *const KBDLLHOOKSTRUCT);
 
-        // キーが押されたとき
-        if wparam.0 as u32 == WM_KEYDOWN {
-            let vk_code = kbd_struct.vkCode;
-            /*
-            if vk_code == 0x41 { // 'A'キーを止める
-                println!("Aキーを黙殺！");
-                return LRESULT(1); // イベントを止める
-            }
-            */
-            while let Ok(vk_code) = rx.recv() {
-                    println!("処理対象のキーボード入力: {}", vk_code);
+pub static KEYBOARD_HANDLE: Lazy<Mutex<Option<HANDLE>>> = Lazy::new(|| {
+    let handle = unsafe {
+        let mut count: u32 = 0;
+        if GetRawInputDeviceList(
+            None,
+            &mut count,
+            std::mem::size_of::<RAWINPUTDEVICELIST>() as u32,
+        ) == u32::MAX {
+            return Mutex::new(None);
+        }
+
+        let mut list = vec![RAWINPUTDEVICELIST::default(); count as usize];
+        if GetRawInputDeviceList(
+            Option::Some(list.as_mut_ptr()),
+            &mut count,
+            std::mem::size_of::<RAWINPUTDEVICELIST>() as u32,
+        ) == u32::MAX {
+            return Mutex::new(None);
+        }
+
+        for dev in list {
+            let mut name_len: u32 = 0;
+            GetRawInputDeviceInfoA(None, RIDI_DEVICENAME, None, &mut name_len);
+
+            let mut name_buf = vec![0u8; name_len as usize];
+            if GetRawInputDeviceInfoA(
+                None,
+                RIDI_DEVICENAME,
+                Some(name_buf.as_mut_ptr() as *mut _),
+                &mut name_len,
+            ) != u32::MAX {
+                let name = String::from_utf8_lossy(&name_buf)
+                    .trim_end_matches('\0')
+                    .to_string();
+                if name.contains(KEYBOARD_PATH.as_str()) {
+                    return Mutex::new(Some(dev.hDevice));
+                }
             }
         }
-    }
 
-    // 次のフックに処理を渡す
-    CallNextHookEx(None, code as i32, wparam, lparam)
-}
+        Mutex::new(None)
+    };
 
-
-
+    handle
+});
 
 
 static mut PRESS_LOG: Option<VecDeque<(String, String)>> = None;
@@ -86,57 +95,53 @@ extern "system" fn wnd_proc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM
                 let rid = [RAWINPUTDEVICE {
                     usUsagePage: 0x01,
                     usUsage: 0x06,     // Keyboard
-                    dwFlags: RIDEV_INPUTSINK, // ウィンドウがフォーカスを失っても入力を受け取る
+                    dwFlags: RIDEV_INPUTSINK | RIDEV_NOLEGACY, // ウィンドウがフォーカスを失っても入力を受け取る
                     hwndTarget: hwnd, // イベント受け取り先
                 }];
 
-                let result = RegisterRawInputDevices(&rid, std::mem::size_of::<RAWINPUTDEVICE>() as u32);
+
+                unsafe {
+                    RegisterRawInputDevices(&rid, rid.len() as u32)
+                        .expect("RawInput登録失敗");
+                }
 
             }
             WM_INPUT => {
-                let mut size = 0u32;
-                GetRawInputData(
-                    HRAWINPUT(lparam.0 as *mut _),
-                    RID_INPUT,
-                    None,
-                    &mut size,
-                    size_of::<RAWINPUTHEADER>() as u32,
-                );
+                let mut size = 0;
+                GetRawInputData(HRAWINPUT(lparam.0 as *mut _), RID_INPUT, None, &mut size, std::mem::size_of::<RAWINPUTHEADER>() as u32);
+                let mut buffer = vec![0u8; size as usize];
+                GetRawInputData(HRAWINPUT(lparam.0 as *mut _), RID_INPUT, Some(buffer.as_mut_ptr() as *mut _), &mut size, std::mem::size_of::<RAWINPUTHEADER>() as u32);
 
-                let mut buf = vec![0u8; size as usize];
-                GetRawInputData(
-                    HRAWINPUT(lparam.0 as *mut _),
-                    RID_INPUT,
-                    Some(buf.as_mut_ptr() as *mut _),
-                    &mut size,
-                    size_of::<RAWINPUTHEADER>() as u32,
-                );
-
-                let raw = &*(buf.as_ptr() as *const RAWINPUT);
+                let raw: &RAWINPUT = &*(buffer.as_ptr() as *const RAWINPUT);
                 if raw.header.dwType == RIM_TYPEKEYBOARD.0 {
-                    let device_handle = raw.header.hDevice;
+                    let dev = raw.header.hDevice;
+                    if ALLOWED_DEVICES.contains(&dev) {
+                        let data = unsafe { raw.data.keyboard() };
+                        let flags = if data.Flags & RI_KEY_BREAK != 0 {
+                            KEYEVENTF_KEYUP
+                        } else {
+                            0
+                        };
 
-                    // まずバッファサイズを取得
-                    let mut size = 0u32;
-                    GetRawInputDeviceInfoA(Some(device_handle), RIDI_DEVICENAME, None, &mut size);
+                        let input = INPUT {
+                            r#type: INPUT_KEYBOARD,
+                            Anonymous: INPUT_0 {
+                                ki: KEYBDINPUT {
+                                    wVk: data.VKey,
+                                    wScan: data.MakeCode,
+                                    dwFlags: flags,
+                                    time: 0,
+                                    dwExtraInfo: 0,
+                                },
+                            },
+                        };
 
-                    // デバイス名取得
-                    let mut buffer = vec![0u8; size as usize];
-                    GetRawInputDeviceInfoA(Some(device_handle), RIDI_DEVICENAME, Some(buffer.as_mut_ptr() as _), &mut size);
-
-                    let device_name = String::from_utf8_lossy(&buffer);
-                    println!("{}", device_name);
-                    if device_name.trim_end_matches(&['\0', '\r', '\n'][..]) == KEYBOARD_HANDLE.as_str() {
-                        println!("いいねぇ");
-                        if let Some(sender) = unsafe { KEY_SENDER.as_ref() } {
-                            sender.send(1).ok();
-                        }
-                    }else{
-                        if let Some(sender) = unsafe { KEY_SENDER.as_ref() } {
-                            sender.send(0).ok();
+                        unsafe {
+                            SendInput(&[input], std::mem::size_of::<INPUT>() as i32);
                         }
                     }
                 }
+                LRESULT(0)
             }
             WM_DESTROY => {
                 PostQuitMessage(0);
@@ -151,39 +156,6 @@ extern "system" fn wnd_proc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM
 const CLASS_NAME: &str = "portapadwindow";
 #[tokio::main]
 async fn main() {
-    // Globalhook処理
-    let globalhookroop = tokio::spawn(async {
-        unsafe {
-            // 現在のプロセスのモジュールハンドルを取得
-            let h_instance = GetModuleHandleW(None).unwrap().into();
-
-            // キーボードフックをセット
-            let hook = SetWindowsHookExW(WH_KEYBOARD_LL, Some(keyboard_proc), Some(h_instance), 0);
-            match hook {
-                Ok(h) => h,
-                Err(e) => {
-                    eprintln!("SetWindowsHookExW に失敗しました。");
-                    return;
-                }
-            };
-            HOOK_HANDLE = hook.unwrap();
-
-            println!("キーボードフック開始中...");
-
-            // メッセージループ
-            let mut msg = MSG::default();
-            while GetMessageW(&mut msg, Some(HWND(null_mut())), 0, 0).into() {
-                TranslateMessage(&msg);
-                DispatchMessageW(&msg);
-            }
-
-            // 終了時にフックを解除
-            UnhookWindowsHookEx(HOOK_HANDLE);
-        }
-    });
-
-    // rawinput処理
-    let rawinputroop = tokio::spawn(async {
         unsafe {
             let h_instance: HINSTANCE = GetModuleHandleA(None).unwrap().into();
 
@@ -221,8 +193,4 @@ async fn main() {
                 DispatchMessageA(&msg);
             }
         }
-    });
-
-    // メインスレッドがすぐ終わらないように待つ
-    let _ = tokio::join!(globalhookroop, rawinputroop);
 }
